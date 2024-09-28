576b86ce299399b74ecf2a8a6b1efa9235817d97
Syed Anees Ibrahim <syedaneesibrahom@gmail.com>
2024-09-27 20:33:29 -0400

    react native setup with typescript

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
